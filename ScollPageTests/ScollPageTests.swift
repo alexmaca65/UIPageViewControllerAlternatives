c6c3e2bd661aa56ba7ebc29ebade911de1b20a95
Alex Maca <alexandrumacarescu@alexs-mac-mini.local>
2016-03-12 19:49:47 +0200

    Initial Commit

---
+++ ScollPageTests/ScollPageTests.swift
@@ -0,0 +1,36 @@
+//
+//  ScollPageTests.swift
+//  ScollPageTests
+//
+//  Created by Alex Maca on 12/03/16.
+//  Copyright © 2016 Alex Maca. All rights reserved.
+//
+
+import XCTest
+@testable import ScollPage
+
+class ScollPageTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
